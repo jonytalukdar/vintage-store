d9194294facdcce7c95b13f34b36726a67c7793f
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-23 23:31:50 +0600

    added useEffect

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -1,5 +1,6 @@
-import { createContext, useState } from 'react';
-
+import axios from 'axios';
+import { createContext, useState, useEffect } from 'react';
+import url from '../utils/URL';
 export const ProductContext = createContext({
   // for auto suggessation
   loading: false,
@@ -12,6 +13,11 @@ const ProductProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [featured, setFeatured] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${url}/products`).then((data) => console.log(data));
+    return () => {};
+  }, []);
 
   const context = {
     loading,
