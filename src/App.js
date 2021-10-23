9a2049aacbed6511a9471b92ae2d618cfd3a5630
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-23 10:50:36 +0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React from 'react';
 
-export default function App() {
-  return <h1>hello from app component</h1>;
-}
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Products from './pages/Products';
+import ProductsDetails from './pages/ProductDetails';
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default App;
