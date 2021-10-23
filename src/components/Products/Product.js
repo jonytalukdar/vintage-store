fbe212bd7f81b2f1e28854fec8b56a35757bc0fd
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-23 23:58:43 +0600

    added productst

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export default function Product() {
+export default function Product({ item }) {
+  const {
+    id,
+    title,
+    description,
+    image: { url },
+  } = item;
   return <h1>hello from product</h1>;
 }
