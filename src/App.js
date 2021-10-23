c2067d9eabca11e9b94e4dfcdab53ab91b1304b0
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-23 11:02:51 +0600

    added react rotuer setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
+//pages
 import About from './pages/About';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
@@ -9,11 +11,19 @@ import Login from './pages/Login';
 import Products from './pages/Products';
 import ProductsDetails from './pages/ProductDetails';
 
+//component
 const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/cart" component={Cart} />
+      <Route exact path="/products" component={Products} />
+      <Route path="/products/:id" component={ProductsDetails} />
+      <Route path="/login" component={Login} />
+      <Route path="/checkout" component={Checkout} />
+      <Route exact path="*" component={Error} />
+    </>
   );
 };
 
