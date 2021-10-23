e4be46c7859e6ba4da96088bf0209ce7b752284b
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-24 00:07:41 +0600

    added signle product

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Product({ item }) {
   const {
     id,
     title,
+    price,
     description,
     image: { url },
   } = item;
-  return <h1>hello from product</h1>;
+  return (
+    <article className="product">
+      <div className="img-container">
+        <img src={url} alt={title} />
+        <Link to={`/products/${id}`} className="btn btn-primary product-link">
+          Details
+        </Link>
+      </div>
+      <div className="product-footer">
+        <p className="product-title">{title}</p>
+        <p className="product-price">{price}</p>
+      </div>
+    </article>
+  );
 }
