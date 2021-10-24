1d977bf2189044e7955eb711c37b377600af3072
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-24 15:17:48 +0600

    add cartLInk

--- src/components/Cart/CartLink.js
+++ src/components/Cart/CartLink.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from '../../context/cart-context';
 
-export default function CartLink() {
-  return <h1>hello from cart link</h1>;
-}
+const CartLink = () => {
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className="cart-link-container">
+      <Link to="/cart">Cart</Link>
+      <span className="cart-link-total">{cartItems}</span>
+    </div>
+  );
+};
+
+export default CartLink;
