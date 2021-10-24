b76cde9e4ff1bea3f3704c6b7208c111c553f62c
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-24 11:57:46 +0600

    added cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -6,11 +6,29 @@ import { Link } from 'react-router-dom';
 // import {UserContext} from '../context/user-context'
 
 export default function Cart() {
+  let user = false;
   const { cart, total } = useContext(CartContext);
 
   if (cart.length === 0) {
     return <EmptyCart />;
   }
 
-  return <h1>hello from cart page</h1>;
+  return (
+    <section className="cart cart-items">
+      <h2>Your Cart</h2>
+      {cart.map((item) => {
+        return <CartItem key={item.id} cart={item} />;
+      })}
+      <h2>Total $ : {total}</h2>
+      {user ? (
+        <Link to="/checkout" className="btn btn-primary btn-block">
+          checkout
+        </Link>
+      ) : (
+        <Link to="/login" className="btn btn-primary btn-block">
+          login
+        </Link>
+      )}
+    </section>
+  );
 }
