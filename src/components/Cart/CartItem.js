6b79cfe601da9b544faced32b171516a5649811b
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-24 12:14:33 +0600

    added single cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React from 'react';
+import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
 
-export default function CartItem() {
-  return <h1>hello form cart item</h1>;
-}
+const CartItem = ({ cart }) => {
+  const { id, image, title, price, amount } = cart;
+  return (
+    <article className="cart-item">
+      <img src={image} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h5>{price}</h5>
+        <button
+          type="button"
+          className="cart-btn remove-btn"
+          onClick={() => console.log('cart removed')}
+        >
+          remove
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => console.log('increase')}
+        >
+          <FaAngleUp />
+        </button>
+        <p className="cart-amount">{amount}</p>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => console.log('decrease')}
+        >
+          <FaAngleDown />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
