250e44832c781686f29adeaff06d222c6d2b2e68
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-25 11:18:43 +0600

    completed login jsx

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('default');
-  const [isMember, setIsMember] = useState(true);
+  const [isMember, setIsMember] = useState(false);
 
   let isEmpty = false;
 
@@ -22,7 +22,67 @@ const Login = () => {
     e.preventDefault();
   };
 
-  return <div>login</div>;
+  return (
+    <section className="form section">
+      <h2 className="section-title">{isMember ? 'Log In' : 'Sign Up'}</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        {/* single input start */}
+        <div className="form-control">
+          <label htmlFor="email">email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        {/* end of single input */}
+
+        {/* single input */}
+        <div className="form-control">
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        {/* end of single input */}
+
+        {/* single input */}
+        {!isMember && (
+          <div className="form-control">
+            <label htmlFor="username">username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+        )}
+        {/* end of single input */}
+        {/* empty form text */}
+        {isEmpty && (
+          <p className="form-empty">please fill out all input feild</p>
+        )}
+        {/* submit button */}
+        {!isEmpty && (
+          <button type="submit" className="btn btn-primary btn-block">
+            submit
+          </button>
+        )}
+        {/* toggle login and sign up */}
+        <p className="register-link">
+          {isMember ? 'Need an account ?' : 'Already have an account ?'}{' '}
+          <button type="button" onClick={isMemberToggleHandler}>
+            Click Here
+          </button>
+        </p>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
