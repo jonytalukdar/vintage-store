a0f97152650ae756ee93aabe425c5596e53573da
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-25 10:50:41 +0600

    added login jsx

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 
-export default function Login() {
-  return <h1>hello from login page</h1>;
-}
+const Login = () => {
+  const history = useHistory();
+
+  //setup user context
+
+  // state
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('default');
+  const [isMember, setIsMember] = useState(true);
+
+  let isEmpty = false;
+
+  const isMemberToggleHandler = () => {
+    setIsMember(!isMember);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return <div>login</div>;
+};
+
+export default Login;
