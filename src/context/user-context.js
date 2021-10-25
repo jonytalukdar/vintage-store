c25fdcfe8b6919d58d966bfa4d91080b2bf1802b
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-25 19:07:07 +0600

    setup alert funtionality

--- src/context/user-context.js
+++ src/context/user-context.js
@@ -13,28 +13,49 @@ export const UserContext = createContext({
   user: {},
   userLogin: (user) => {},
   userLogout: () => {},
+  alert: {},
+  showAlert: () => {},
+  hideAlert: () => {},
 });
 
 const UserProvider = ({ children }) => {
   //   const [user, setUser] = useState({ username: null, token: null });
   const [user, setUser] = useState(getUserFromLocalStorage());
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: 'success',
+  });
 
-  console.log(user);
-
+  //login
   const userLogin = (user) => {
     setUser(user);
     localStorage.setItem('user', JSON.stringify(user));
   };
 
+  // logout
   const userLogout = () => {
     setUser({ username: null, token: null });
     localStorage.removeItem('user');
+  };
+
+  // show alert
+  const showAlert = ({ msg, type = 'success' }) => {
+    setAlert({ show: true, msg, type });
+  };
+
+  //hide alert
+  const hideAlert = () => {
+    setAlert({ ...alert, show: false });
   };
 
   const context = {
     user,
     userLogin,
     userLogout,
+    alert,
+    showAlert,
+    hideAlert,
   };
 
   return (
