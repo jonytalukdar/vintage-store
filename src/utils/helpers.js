f22b7660f43033408ebd58d1c09becb1b10c2d39
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-31 11:37:27 +0600

    added paginate splice

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8,6 +8,12 @@ export const featuredProducts = (data) => {
 // paginate function
 
 export const paginate = (products) => {
+  const itemsPerPage = 3;
+  const numberOfPage = Math.ceil(products.length / itemsPerPage);
+  const newProducts = Array.from({ length: numberOfPage }, () => {
+    return products.splice(0, itemsPerPage);
+  });
+  console.log(newProducts);
   // our code goes here
 
   return products;
